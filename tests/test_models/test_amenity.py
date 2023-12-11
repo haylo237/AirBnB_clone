bda45731d763b6c9bda390115301ebe1c00d6c1e
Partrixs <patricknjoroge092@gmail.com>
2023-12-11 12:19:36 +0300

    new commit

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -113,6 +113,7 @@ class TestAmenity_save(unittest.TestCase):
         am.save()
         self.assertLess(first_updated_at, am.updated_at)
 
+
     def test_two_saves(self):
         am = Amenity()
         sleep(0.05)
@@ -124,10 +125,12 @@ class TestAmenity_save(unittest.TestCase):
         am.save()
         self.assertLess(second_updated_at, am.updated_at)
 
+
     def test_save_with_arg(self):
         am = Amenity()
         with self.assertRaises(TypeError):
             am.save(None)
+
 
     def test_save_updates_file(self):
         am = Amenity()
@@ -143,12 +146,14 @@ class TestAmenity_to_dict(unittest.TestCase):
     def test_to_dict_type(self):
         self.assertTrue(dict, type(Amenity().to_dict()))
 
+
     def test_to_dict_contains_correct_keys(self):
         am = Amenity()
         self.assertIn("id", am.to_dict())
         self.assertIn("created_at", am.to_dict())
         self.assertIn("updated_at", am.to_dict())
         self.assertIn("__class__", am.to_dict())
+
 
     def test_to_dict_contains_added_attributes(self):
         am = Amenity()
@@ -157,12 +162,14 @@ class TestAmenity_to_dict(unittest.TestCase):
         self.assertEqual("Holberton", am.middle_name)
         self.assertIn("my_number", am.to_dict())
 
+
     def test_to_dict_datetime_attributes_are_strs(self):
         am = Amenity()
         am_dict = am.to_dict()
         self.assertEqual(str, type(am_dict["id"]))
         self.assertEqual(str, type(am_dict["created_at"]))
         self.assertEqual(str, type(am_dict["updated_at"]))
+
 
     def test_to_dict_output(self):
         dt = datetime.today()
@@ -177,9 +184,11 @@ class TestAmenity_to_dict(unittest.TestCase):
         }
         self.assertDictEqual(am.to_dict(), tdict)
 
+
     def test_contrast_to_dict_dunder_dict(self):
         am = Amenity()
         self.assertNotEqual(am.to_dict(), am.__dict__)
+
 
     def test_to_dict_with_arg(self):
         am = Amenity()
