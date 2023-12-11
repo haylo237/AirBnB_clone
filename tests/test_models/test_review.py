700d547098bd631db55f93ddf2f62f6c2a6aafb5
Partrixs <patricknjoroge092@gmail.com>
2023-12-11 12:24:01 +0300

    new commit

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -153,12 +153,14 @@ class TestReview_to_dict(unittest.TestCase):
     def test_to_dict_type(self):
         self.assertTrue(dict, type(Review().to_dict()))
 
+
     def test_to_dict_contains_correct_keys(self):
         rv = Review()
         self.assertIn("id", rv.to_dict())
         self.assertIn("created_at", rv.to_dict())
         self.assertIn("updated_at", rv.to_dict())
         self.assertIn("__class__", rv.to_dict())
+
 
     def test_to_dict_contains_added_attributes(self):
         rv = Review()
@@ -167,12 +169,14 @@ class TestReview_to_dict(unittest.TestCase):
         self.assertEqual("Holberton", rv.middle_name)
         self.assertIn("my_number", rv.to_dict())
 
+
     def test_to_dict_datetime_attributes_are_strs(self):
         rv = Review()
         rv_dict = rv.to_dict()
         self.assertEqual(str, type(rv_dict["id"]))
         self.assertEqual(str, type(rv_dict["created_at"]))
         self.assertEqual(str, type(rv_dict["updated_at"]))
+
 
     def test_to_dict_output(self):
         dt = datetime.today()
@@ -190,6 +194,7 @@ class TestReview_to_dict(unittest.TestCase):
     def test_contrast_to_dict_dunder_dict(self):
         rv = Review()
         self.assertNotEqual(rv.to_dict(), rv.__dict__)
+
 
     def test_to_dict_with_arg(self):
         rv = Review()
